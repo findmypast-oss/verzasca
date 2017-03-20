79a729b7ff821ce1f7bcb77ac40cac89648fb7ff
Neil Crawford <neil.crawford@gmail.com>
2017-03-20 13:28:54 +0000

    Removing uneeded files

--- test/globals.js
+++ test/globals.js
@@ -1,13 +1,9 @@
 const chai = require('chai');
 global.should = chai.should();
 
-// const chaiAsPromised = require('chai-as-promised');
-// chai.use(chaiAsPromised);
-
 global.sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
 global.expect = chai.expect;
 global.mockery = require('mockery');
-// global.Promise = require('bluebird');
