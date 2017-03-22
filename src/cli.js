1d9f159175b7e6a000d37a7b01caa14b36dbbcf5
Neil Crawford <neil.crawford@gmail.com>
2017-03-22 10:01:09 +0000

    Removing testMode from the CLI

--- src/cli.js
+++ src/cli.js
@@ -8,7 +8,6 @@ program
   .option('--url <url>', 'Set teamcity url')
   .option('--builds <builds...>', 'The builds to check the status of (comma separated)', x => x.split(','))
   .option('--auth <auth>', 'Basic auth token for teamcity')
-  .option('--test-mode', 'Run in test mode')
   .parse(process.argv)
 
 if (!process.argv.slice(2).length) {
